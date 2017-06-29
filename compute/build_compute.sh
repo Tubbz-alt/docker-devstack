d9b76b74330e24af6c44647a1fbda93260375473
Matt Welch <matt.welch@intel.com>
2017-06-28 18:13:18 -0700

    enable building compute container v0.5
    Rebuilt compute container based on updated Dockerfile intended to be
    used with OpenDaylight Carbon SR1.
    
    Compute container v0.5 successfully joined a cluster running
    OpenDaylight Carbon SR1 as network controller.
    
    Refactored compute container directory to keep container configuration &
    script files in the build/ directory.
    
    Run- and build-scripts will now consume environment variables or use a
    set of known-good defaults.
    
    The run_compute.sh will now also connect the container to the bridged
    network by default using
    docker-devstack/docker/connect_container_to_networks.sh

--- compute/build_compute.sh
+++ compute/build_compute.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # file: build_compute.sh
 # info: builds a docker compute image
+IMAGE_REGISTRY=${IMAGE_REGISTRY:-"matt-registry:4000"}
 IMAGE_REPO=${IMAGE_REPO:-s3p/compute}
 IMAGE_TAG=${IMAGE_TAG:-latest}
 
@@ -8,18 +9,18 @@ if [ -n "$1" ] ; then
     # use arg as image tag if supplied
     IMAGE_TAG="$1"
 fi
-IMAGE_NAME=${IMAGE_REPO}:${IMAGE_TAG}
-DOCKERFILE=${DOCKERFILE:-"compute.ubuntu1604.Dockerfile"}
+IMAGE_NAME=${IMAGE_REGISTRY}/${IMAGE_REPO}:${IMAGE_TAG}
+DOCKERFILE=${DOCKERFILE:-"./build/compute.Dockerfile"}
 
 echo "Building $IMAGE_NAME from Dockerfile=$DOCKERFILE at $(date) ... "
 docker build -t ${IMAGE_NAME} -f ${DOCKERFILE} \
-    --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy .
+    --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy ./build
 
 if [ $? = 0 ] ; then
     docker images $IMAGE_NAME
     echo "Docker image $IMAGE_NAME built successfully."
     echo "To quickly test it, you can launch it with:"
-    echo "docker run -it --rm --env http_proxy=$http_proxy --env https_proxy=$https_proxy --env no_proxy=$no_proxy $IMAGE_NAME bash"
+    echo "docker run -it --rm --env http_proxy=$http_proxy --env https_proxy=$https_proxy --env no_proxy=$no_proxy $IMAGE_NAME /bin/bash"
 else
     echo "An error occurred during the build of $IMAGE_NAME"
 fi
--- compute/run_compute.sh
+++ compute/run_compute.sh
@@ -6,58 +6,50 @@
 # + Compute host image must also be available locally or in a registry.
 
 # image selection
-IMAGE_REPO=${IMAGE_REPO:-ss3p/compute}
-IMAGE_TAG=${IMAGE_TAG:-v0.4}
-IMAGE_NAME="${IMAGE_REPO}:${IMAGE_TAG}"
+IMAGE_REGISTRY=${IMAGE_REGISTRY:-"matt-registry:4000"}
+IMAGE_REPO=${IMAGE_REPO:-s3p/compute}
+IMAGE_TAG=${IMAGE_TAG:-v0.5} # env
+IMAGE_NAME="${IMAGE_REGISTRY}/${IMAGE_REPO}:${IMAGE_TAG}"
 
 # image configuration
-HOST_ID=01
-COMP_ID=05
-NAME=${HOST_NAME:-compute-${HOST_ID}-${COMP_ID}}
-ODL_NETWORK=${ODL_NETWORK:-True}
-# when running on a RHEL-derivative host, use "--security-opt seccomp=unconfined"
+HOST_ID=${HOST_ID:-99} # should be in the env
+COMP_ID=${COMP_ID:-11}
+NAME=${CONTAINER_NAME:-compute-${HOST_ID}-${COMP_ID}}
 CAPABILITIES="--privileged --cap-add ALL --security-opt apparmor=docker-unconfined "
 SYSTEMD_ENABLING=" --tmpfs /run --tmpfs /run/lock --tmpfs /run/uuid --stop-signal=SIGRTMIN+3 "
 CGROUP_MOUNT=" -v /sys/fs/cgroup:/sys/fs/cgroup:ro "
 MOUNTS="-v /dev:/dev -v /lib/modules:/lib/modules $CGROUP_MOUNT $SYSTEMD_ENABLING "
-STACK_PASS=${STACK_PASS:-stack}
-# default SERVICE_HOST, based on openstack. This may be overridden.
-SERVICE_HOST=${SERVICE_HOST:-192.168.3.2}
-# define _no_proxy based on the cluster topology
-_no_proxy=localhost,10.0.0.0/8,192.168.0.0/16,172.17.0.0/16,127.0.0.1,127.0.0.0/8,$SERVICE_HOST
-PORT_MAP=""
-# NETWORK_NAME=${NETWORK_NAME:-"mgmt-net"}
-NETWORK_SETTINGS=" $PORT_MAP" # --net=$NETWORK_NAME
 
-# TODO: automatically detect the service-node here and set it's IP: SERVICE_HOST=$SERVICE_NODE_IP
-# SERVICE_NODE_NAME=${SERVICE_HOST_NAME:-"service-node"}
-# # check to see that service-node is running first and get its IP from Docker
-# SERVICE_NODE_IP=$(docker inspect -f "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" $SERVICE_NODE_NAME)
-# if [ -z "$SERVICE_NODE_IP" ] ; then
-#     echo "WARNING: no service node is available on overlay-net."
-#     echo "You can launch the compute container, but it may not be able to connect to a service node."
-# else
-# 
-# fi
+# Container environment and OpenStack Config
+STACK_USER=${STACK_USER:-stack}
+STACK_PASS=${STACK_PASS:-stack}
+ODL_NETWORK=${ODL_NETWORK:-True}
+SERVICE_HOST_NAME=service-node
+SERVICE_HOST=${SERVICE_HOST:-10.129.19.2}
+NO_PROXY=localhost,10.0.0.0/8,192.168.0.0/16,172.17.0.0/16,127.0.0.1,127.0.0.0/8,$SERVICE_HOST
+
 docker run -dit --name ${NAME} --hostname ${NAME} \
     --env http_proxy=$http_proxy --env https_proxy=$https_proxy \
-    --env no_proxy=$_no_proxy \
+    --env no_proxy=$NO_PROXY \
     --env ODL_NETWORK=$ODL_NETWORK \
     --env STACK_PASS=$STACK_PASS \
     --env SERVICE_HOST=$SERVICE_HOST \
     --env container=docker \
-    $NETWORK_SETTINGS \
     $MOUNTS \
     $CAPABILITIES \
     $IMAGE_NAME \
     /sbin/init
 
+# connect containers to host bridges (assumes bridges named br_data and br_mgmt exist on the host
+../docker/connect_container_to_networks.sh $HOSTNAME $COMP_ID compute
+
 AUTO_STACK=no
 if [[ "$AUTO_STACK" == "no" ]] ; then
     COMMAND='/bin/bash'
 else
-    COMMAND='su -c "/home/stack/start.sh" stack'
+    COMMAND='/bin/bash -c "/home/stack/start.sh"'
 fi
 CONTAINER_SHORT_ID=$(docker ps -aqf "name=${NAME}")
-docker exec -it $CONTAINER_SHORT_ID "$COMMAND"
+echo "Now entering container: $NAME"
+docker exec -it -u $STACK_USER $CONTAINER_SHORT_ID "$COMMAND"
 
